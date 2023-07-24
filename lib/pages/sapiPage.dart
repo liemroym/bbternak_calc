82cf72f91a2eece5a91226feed47c1cc0643bcbf
liemroym <liemroymarcelino@gmail.com>
2023-07-24 16:58:04 +0700

    Turned fetching to list of integer

--- lib/pages/sapiPage.dart
+++ lib/pages/sapiPage.dart
@@ -17,13 +17,85 @@ class SapiPage extends StatefulWidget {
 }
 
 class _SapiPageState extends State<SapiPage> {
+  List<int?>? priceJateng, priceKlaten;
+
+  String formatDate(DateTime dateTime, {bool reversed = true}) {
+    String day = dateTime.day.toString().padLeft(2, '0');
+    String month = dateTime.month.toString().padLeft(2, '0');
+    String year = dateTime.year.toString();
+
+    if (!reversed) {
+      return '$day-$month-$year';
+    } else {
+      return '$year-$month-$day';
+    }
+  }
+
+  Future<Map<String, List<int?>?>> fetchPriceData(
+      DateTime dateStart, DateTime dateEnd) async {
+    int dateDiff = dateEnd.difference(dateStart).inDays;
+
+    final response = await http.post(
+      Uri.parse(
+          'https://simponiternak.pertanian.go.id/download-harga-komoditas.php?i_laporan=1&data_tanggal_1=${formatDate(dateStart, reversed: false)}&data_tanggal_2=${formatDate(dateEnd, reversed: false)}&data_bulan_1={2012-01}&data_bulan_2=${formatDate(dateEnd).substring(0, 7)}&data_tahun_1=2012&data_tahun_2=${formatDate(dateEnd).substring(0, 4)}&i_komoditas=3&i_type=1&i_showcity=provkab&i_kabupaten[]=3310&i_provinsi[]=33&'),
+    );
+
+    final bytes = response.bodyBytes;
+    var excel = Excel.decodeBytes(bytes);
+    var sheet = excel.tables[excel.tables.keys.first];
+
+    List<String?>? pricesJateng = sheet?.rows[9]
+        .getRange(4, 4 + dateDiff)
+        .map((e) => e?.value.toString())
+        .toList();
+    List<String?>? pricesKlaten = sheet?.rows[10]
+        .getRange(4, 4 + dateDiff)
+        .map((e) => e?.value.toString())
+        .toList();
+
+    List<int?>? pricesJatengConverted = pricesJateng
+        ?.map((e) => (e == null
+            ? null
+            : (DateTime.parse(e).difference(DateTime(1900, 1, 1)).inDays + 2)
+                .toString()))
+        .map((e) => e == null ? null : int.tryParse(e))
+        .toList();
+
+    List<int?>? pricesKlatenConverted = pricesKlaten
+        ?.map((e) => (e == null
+            ? null
+            : (DateTime.parse(e).difference(DateTime(1900, 1, 1)).inDays + 2)
+                .toString()))
+        .map((e) => e == null ? null : int.tryParse(e))
+        .toList();
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      // return prices.join("\n");
+      return {
+        "hargaJateng": pricesJatengConverted,
+        "hargaKlaten": pricesKlatenConverted
+      };
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
   @override
   void initState() {
     super.initState();
-    data = "";
+    fetchPriceData(
+            DateTime.now().subtract(const Duration(days: 7)), DateTime.now())
+        .then((value) => {
+              setState(() {
+                priceJateng = value["priceJateng"];
+                priceKlaten = value["priceKlaten"];
+              })
+            });
   }
-
-  String data = "";
 
   @override
   Widget build(BuildContext context) {
@@ -57,87 +129,6 @@ class _SapiPageState extends State<SapiPage> {
       "lingkarDadaCm": TextEditingController()
     };
 
-    String formatDate(DateTime dateTime, {bool reversed = true}) {
-      String day = dateTime.day.toString().padLeft(2, '0');
-      String month = dateTime.month.toString().padLeft(2, '0');
-      String year = dateTime.year.toString();
-
-      if (!reversed) {
-        return '$day-$month-$year';
-      } else {
-        return '$year-$month-$day';
-      }
-    }
-
-    Future<String> fetchPriceData(DateTime dateStart, DateTime dateEnd) async {
-      setState((() => data = "Fetching value"));
-
-      int dateDiff = dateEnd.difference(dateStart).inDays;
-
-      final response = await http.post(
-        Uri.parse(
-            'https://simponiternak.pertanian.go.id/download-harga-komoditas.php?i_laporan=1&data_tanggal_1=${formatDate(dateStart, reversed: false)}&data_tanggal_2=${formatDate(dateEnd, reversed: false)}&data_bulan_1={2012-01}&data_bulan_2=${formatDate(dateEnd).substring(0, 7)}&data_tahun_1=2012&data_tahun_2=${formatDate(dateEnd).substring(0, 4)}&i_komoditas=3&i_type=1&i_showcity=provkab&i_kabupaten[]=3310&i_provinsi[]=33&'),
-      );
-
-      final bytes = response.bodyBytes;
-      var excel = Excel.decodeBytes(bytes);
-      var sheet = excel.tables[excel.tables.keys.first];
-
-      print("${formatDate(dateStart)} ${formatDate(dateEnd)}");
-
-      List<String?>? pricesJateng = sheet?.rows[9]
-          .getRange(4, 4 + dateDiff)
-          .map((e) => e?.value.toString())
-          .toList();
-      List<String?>? pricesKlaten = sheet?.rows[10]
-          .getRange(4, 4 + dateDiff)
-          .map((e) => e?.value.toString())
-          .toList();
-
-      pricesJateng = pricesJateng
-          ?.map((e) => (e == null
-              ? null
-              : (DateTime.parse(e).difference(DateTime(1900, 1, 1)).inDays + 2)
-                  .toString()))
-          .toList();
-
-      pricesKlaten = pricesKlaten
-          ?.map((e) => e == null
-              ? null
-              : (DateTime.parse(e).difference(DateTime(1900, 1, 1)).inDays + 2)
-                  .toString())
-          .toList();
-
-      // print(sheet?.rows[9].getRange(4, dateDiff));
-
-      // for (var row in sheet!.rows) {
-      //   // Get dates
-      //   print("${row.first?.value} ${row.first?.value.runtimeType}");
-      //   if (row.first?.value == "No") {
-      //     dates = row.getRange(4, 4 + dateDiff).cast<String>().toList();
-      //   }
-      // }
-
-      if (response.statusCode == 200) {
-        // If the server did return a 200 OK response,
-        // then parse the JSON.
-        // return prices.join("\n");
-        return pricesKlaten!.join(" ");
-      } else {
-        // If the server did not return a 200 OK response,
-        // then throw an exception.
-        throw Exception('Failed to load album');
-      }
-    }
-
-    void onPressed() async {
-      fetchPriceData(
-              DateTime.now().subtract(const Duration(days: 7)), DateTime.now())
-          .then((value) => {setState((() => data = value))});
-    }
-
-    // fetchAlbum().then((value) => {setState((() => data = value))});
-
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the KambingPage object that was created by
@@ -166,8 +157,6 @@ class _SapiPageState extends State<SapiPage> {
           calcFunc: smith,
           sharedControllers: sharedControllers,
         ),
-        ElevatedButton(onPressed: onPressed, child: Text("Pressed")),
-        Text(data),
       ]),
     );
   }
