7ffcfbf6530db26440eddd3a811d2fc95f236b54
liemroym <liemroymarcelino@gmail.com>
2023-07-30 06:30:50 +0700

    Changed name

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BBTernak Calculator',
+      title: 'BBTernak Kalkulator',
       theme: ThemeData(
         fontFamily: "Poppins",
         primarySwatch: Colors.green,
